e8c883324871c87c41edd9430913ebb9ee838d6a
Arun Kumar Nair <arunnair018@gmail.com>
2023-07-03 06:34:58 +0530

    added url, view and template

--- djangokickstart/urls.py
+++ djangokickstart/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('todo', include("todo_app.urls")),
+    path('todo/', include("todo_app.urls")),
 ]
